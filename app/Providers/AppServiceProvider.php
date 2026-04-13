37dfc6a4f200854a58c3ea2348a23f038ca3f62f
masterMohamedsaleh <ms99632147@gmail.com>
2026-04-13 22:49:58 +0200

    add config env

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -46,6 +46,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        Paginator::useBootstrap();
+       if (config('app.env') === 'production') {
+        \URL::forceScheme('https');
+    }
     }
 }
